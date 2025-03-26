fc49acbab7392991ec9e947a109611756a0f13e7
rubennmg <rubenmg2015@gmail.com>
2025-03-26 19:45:01 +0100

    Implement run_phase_3. Use fill_matrix to fill C with 0's

--- sesion2/multiply_matrices_hybrid_pro.py
+++ sesion2/multiply_matrices_hybrid_pro.py
@@ -1,8 +1,8 @@
 import ctypes
 import sys
-import random
 import time
-from typing import List
+from utils import verify_multiplication, print_matrix, matrix_to_python
+from typing import List, Dict, Callable
 
 type matrix = List[List[int]]
 
@@ -20,6 +20,10 @@ lib.free_matrix.restype = None
 # generate_matrix function prototype
 lib.generate_matrix.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.POINTER(ctypes.c_int))]
 lib.generate_matrix.restype = None
+
+# fill_matrix function prototype
+lib.fill_matrix.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.POINTER(ctypes.c_int)), ctypes.c_int]
+lib.fill_matrix.restype = None
 
 # Common arguments for matrix multiplication functions
 common_args = [
@@ -41,25 +45,52 @@ lib.column_major_mul.restype = None
 # zorder_mul function prototype
 lib.zorder_mul.argtypes = common_args + [ctypes.c_int]
 lib.zorder_mul.restype = None
-
-def matrix_to_python(matrix_c: matrix, rows: int, columns: int) -> matrix:
-    """Convert a matrix in C format to a list of Python lists."""
-    matrix = [[0] * columns for _ in range(rows)]
-    for i in range(rows):
-        for j in range(columns):
-            matrix[i][j] = matrix_c[i][j]
-    return matrix
-
-def print_matrix(matrix: matrix) -> None:
-    """Prints a matrix."""
-    if len(matrix) > 10:
-        print("Matrix too large to print\n")
-        return
     
-    for row in matrix:
-        print(row)
-    print()
-
+def run_phase_3(matrix_size: int, block_size: int) -> Dict[str, float]:
+    """Run phase 3 of the experimient"""
+    
+    def measure_time(mul_func: Callable, matrix_size: int, a: matrix, b: matrix, a_python: matrix, b_python: matrix, block_size_arg: int = None) -> float:
+        """Measure the time of a matrix multiplication function."""
+        C = lib.allocate_matrix(matrix_size, matrix_size)
+        
+        lib.fill_matrix(matrix_size, matrix_size, C, 0)
+        
+        if mul_func == lib.zorder_mul:
+            start = time.time()
+            mul_func(matrix_size, matrix_size, a, b, C, block_size_arg)
+        else:
+            start = time.time()
+            mul_func(matrix_size, matrix_size, a, b, C)
+        
+        exec_time = time.time() - start
+        
+        c_python = matrix_to_python(C, matrix_size, matrix_size)
+        assert verify_multiplication(a_python, b_python, c_python), f"Error in {mul_func.__name__}!"
+        
+        lib.free_matrix(matrix_size, C)
+        
+        return exec_time
+    
+    A = lib.allocate_matrix(matrix_size, matrix_size)
+    B = lib.allocate_matrix(matrix_size, matrix_size)
+    
+    lib.generate_matrix(matrix_size, matrix_size, A)
+    lib.generate_matrix(matrix_size, matrix_size, B)
+    
+    a_python = matrix_to_python(A, matrix_size, matrix_size)
+    b_python = matrix_to_python(B, matrix_size, matrix_size)
+    
+    times = {
+        "Row-major order": measure_time(lib.row_major_mul, matrix_size, A, B, a_python, b_python),
+        "Column-major order": measure_time(lib.column_major_mul, matrix_size, A, B, a_python, b_python),
+        "Z order": {block_size: measure_time(lib.zorder_mul, matrix_size, A, B, a_python, b_python, block_size_arg=block_size)}
+    }
+    
+    lib.free_matrix(matrix_size, A)
+    lib.free_matrix(matrix_size, B)
+    
+    return times
+    
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python 2-multiply_matrices.py <rows> <columns> <block_size>")
@@ -80,6 +111,11 @@ if __name__ == "__main__":
     # Generate random matrices
     lib.generate_matrix(rows, columns, A)
     lib.generate_matrix(rows, columns, B)
+    
+    # Fill result matrices with zeros
+    lib.fill_matrix(rows, columns, C_rows, 0)
+    lib.fill_matrix(rows, columns, C_columns, 0)
+    lib.fill_matrix(rows, columns, C_zorder, 0)
 
     # Row-major order matrix multiplication
     start = time.time()
@@ -101,3 +137,10 @@ if __name__ == "__main__":
     print(f"Z order: {time.time() - start:.6f} s")
     C_p_zorder = matrix_to_python(C_zorder, rows, columns)
     print_matrix(C_p_zorder)
+    
+    # Free matrices
+    lib.free_matrix(rows, A)
+    lib.free_matrix(rows, B)
+    lib.free_matrix(rows, C_rows)
+    lib.free_matrix(rows, C_columns)
+    lib.free_matrix(rows, C_zorder)
