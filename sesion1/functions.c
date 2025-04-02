0c521d4a233c98b7265947a59662e120aad63ab4
rubennmg <rubenmg2015@gmail.com>
2025-04-02 18:17:02 +0200

    Better sesion1 docs

--- sesion1/functions.c
+++ sesion1/functions.c
@@ -1,15 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include "functions.h"
 
-/**
- * Prints a matrix.
- * @param rows Number of rows in the matrix.
- * @param columns Number of columns in the matrix.
- * @param matrix Pointer to the matrix to be printed.
- */
 void print_matrix(int rows, int columns, int **matrix)
 {
     for (int i = 0; i < rows; i++)
@@ -24,14 +14,6 @@ void print_matrix(int rows, int columns, int **matrix)
     printf("\n");
 }
 
-/**
- * Prints the product of two matrices.
- * @param A Pointer to the first matrix.
- * @param B Pointer to the second matrix.
- * @param C Pointer to the resulting matrix.
- * @param rows Number of rows in the matrices.
- * @param columns Number of columns in the matrices.
- */
 void print_product(int **A, int **B, int **C, int rows, int columns)
 {
     if (rows > 10 || columns > 10)
@@ -77,10 +59,6 @@ void print_product(int **A, int **B, int **C, int rows, int columns)
     }
 }
 
-/**
- * Checks if the matrix was allocated correctly.
- * @param matrix Pointer to the matrix to be checked.
- */
 void check_err(int **matrix)
 {
     if (matrix == NULL)
--- sesion1/functions.h
+++ sesion1/functions.h
@@ -1,8 +1,33 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/**
+ * Prints a matrix.
+ * @param rows Number of rows in the matrix.
+ * @param columns Number of columns in the matrix.
+ * @param matrix Pointer to the matrix to be printed.
+ */
 void print_matrix(int rows, int columns, int **matrix);
+
+/**
+ * Prints the product of two matrices.
+ * @param A Pointer to the first matrix.
+ * @param B Pointer to the second matrix.
+ * @param C Pointer to the resulting matrix.
+ * @param rows Number of rows in the matrices.
+ * @param columns Number of columns in the matrices.
+ */
 void print_product(int **A, int **B, int **C, int rows, int columns);
+
+/**
+ * Checks if the matrix was allocated correctly.
+ * @param matrix Pointer to the matrix to be checked.
+ */
 void check_err(int **matrix);
 
 #endif
